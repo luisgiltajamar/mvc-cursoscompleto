8fcd22a25add293f565a44f7ab538bc95d99adeb
luisgiltajamar <luis.gil@tajamar365.com>
2014-11-27 09:22:17 +0100

    e

---
+++ GestionCursos/Controllers/CursoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GestionCursos.Models.ViewModels;
+using GestionCursos.Repositorios;
+
+namespace GestionCursos.Controllers
+{
+    public class CursoController : BaseController
+    {
+        // GET: Curso
+        public ActionResult Index()
+        {
+            var data = RepositorioCursos.Get();
+            return View(data);
+        }
+
+        public ActionResult Alta()
+        {
+            ViewBag.profesor=new SelectList(RepositorioProfesor.Get(),"idProfesor","nombre");
+            ViewBag.idAulas = new MultiSelectList(RepositorioAula.Get(), "idAula", "Nombre");
+            return View(new CursoViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(CursoViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                RepositorioCursos.Add(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
